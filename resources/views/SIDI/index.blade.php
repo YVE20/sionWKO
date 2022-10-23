823c2c06d274d558e6f6d6654a33c6394a3b1c62
irfanvarren <irfanvarren7@gmail.com>
2022-10-24 01:21:37 +0700

    layout to Layouts

--- resources/views/SIDI/index.blade.php
+++ resources/views/SIDI/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app',['title' => 'Sion WKO| SIDI'])
+@extends('Layouts.app',['title' => 'Sion WKO| SIDI'])
 @section('content')
 @include('KategoriIbadah.modal')
 <div class="col-md-12 p-3">
